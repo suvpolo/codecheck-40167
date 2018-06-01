64c96811e07cd336930d67a330af9f71ad29b791
code-project <dev@code-check.io>
2018-06-01 11:18:38 +0000

    Add new CLI related files

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // start from 1 to ignore script name; argv[0] will be a name of processing file.    
-    char data[100];
-    cout<<"Hello "<<argv[1]<<"!\n";
+  // start from 1 to ignore script name; argv[0] will be a name of processing file.
+  for (int i = 1; i < argc; i++) {
+    printf ("argv[%i]: %s\n", i, argv[i]);
+  }
   return 0;
 }
