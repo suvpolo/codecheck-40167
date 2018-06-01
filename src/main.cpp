93b193d3a60e5e2febafc6bcff7a77499689dfcc
code-project <dev@code-check.io>
2018-06-01 11:18:38 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  // start from 1 to ignore script name; argv[0] will be a name of processing file.    
+    char data[100];
+    cout<<"Hello "<<argv[1]<<"!\n";
+  return 0;
+}
